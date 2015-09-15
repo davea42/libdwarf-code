aa1a11d1bfc59d6dc8f9ad4a66781b5987683bef
David Anderson <davea42@earthlink.net>
2015-09-15 16:03:10 -0700

    Updated dwarfdump version stamps.

--- dwarfdump/common.c
+++ dwarfdump/common.c
@@ -33,7 +33,7 @@
 #include "stdafx.h"
 #endif /* HAVE_STDAFX_H */
 
-#define DWARFDUMP_VERSION " Sun Sep 13 16:08:10 PDT 2015  "
+#define DWARFDUMP_VERSION " Tue Sep 15 16:02:57 PDT 2015  "
 #define RELEASE_DATE      "20150507"
 
 /* The Linux/Unix version does not want a version string to print
