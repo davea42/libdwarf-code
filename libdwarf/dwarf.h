d33912b46f364ed053a9a47b91d9c1ea379475af
David Anderson <davea42@earthlink.net>
2015-11-15 10:32:20 -0800

    With latest experimental two-level line tables. dwarf.h

--- libdwarf/dwarf.h
+++ libdwarf/dwarf.h
@@ -990,11 +990,11 @@ extern "C" {
 #define DW_LNS_set_isa                  0x0c /* DWARF3 */
 
 /*  Experimental two-level line tables. NOT STD DWARF5 */
-/*  MISNAMED: not saying GNU or anything. There are no
+/*  Not saying GNU or anything. There are no
     DW_LNS_lo_user or DW_LNS_hi_user values though.
     DW_LNS_set_address_from_logical and
-    DW_LNS_set_subprogram being both 0xd seems...wrong
-    because it makes DW_LNS_* spelling context dependent..
+    DW_LNS_set_subprogram being both 0xd 
+    to avoid using up more space in the special opcode table.
     EXPERIMENTAL DW_LNS follow.
 */
 #define DW_LNS_set_address_from_logical 0x0d /* Actuals table only */
