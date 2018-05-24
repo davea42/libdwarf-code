74a4a847f3d4dddc27cff8d4c8a66d63e7633a2c
David Anderson <davea42@linuxmail.org>
2018-05-24 15:56:54 -0700

    more loads of the same sections (in another function). 	modified:   libdwarf/dwarf_arange.c

--- libdwarf/dwarf_arange.c
+++ libdwarf/dwarf_arange.c
@@ -426,6 +426,17 @@ _dwarf_get_aranges_addr_offsets(Dwarf_Debug dbg,
     if (res != DW_DLV_OK) {
         return res;
     }
+    /*  aranges points in to info, so if info needs expanding
+        we have to load it. */
+    res = _dwarf_load_section(dbg, &dbg->de_debug_info, error);
+    if (res != DW_DLV_OK) {
+        return res;
+    }
+    res = _dwarf_load_section(dbg, &dbg->de_debug_abbrev, error);
+    if (res != DW_DLV_OK) {
+        return res;
+    }
+
 
     res = dwarf_get_aranges_list(dbg,&head_chain,&arange_count,error);
     if (res != DW_DLV_OK) {
