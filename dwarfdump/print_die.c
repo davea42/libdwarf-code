d5926a9dde1222f8f4bd5b799e4ed6d6852349e9
David Anderson <davea42@linuxmail.org>
2018-12-24 11:14:36 -0800

    DO NOT USE. work in progress. 	modified:   dwarfdump/print_die.c

--- dwarfdump/print_die.c
+++ dwarfdump/print_die.c
@@ -5999,8 +5999,8 @@ show_form_itself(int local_show_form,
     }
 }
 
-#include "tmp-ta-table.h"
-#include "tmp-ta-ext-table.h"
+#include "dwarf-ta-table.h"
+#include "dwarf-ta-ext-table.h"
 
 static int
 legal_tag_attr_combination(Dwarf_Half tag, Dwarf_Half attr)
@@ -6051,8 +6051,8 @@ legal_tag_attr_combination(Dwarf_Half tag, Dwarf_Half attr)
     return (FALSE);
 }
 
-#include "tmp-tt-table.h"
-#include "tmp-tt-ext-table.h"
+#include "dwarf-tt-table.h"
+#include "dwarf-tt-ext-table.h"
 
 /*  Look only at valid table entries
     The check here must match the building-logic in
