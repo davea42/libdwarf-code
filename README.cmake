c0ad0dfe48526204105077c39ddc8f5cae9a2848
David Anderson <davea42@linuxmail.org>
2019-04-27 07:19:55 -0700

    Minor changes of the wording. 	modified:   README.cmake

--- README.cmake
+++ README.cmake
@@ -7,22 +7,32 @@ The FIX-COMPILE-TIMES script is irrelevant to cmake.
 The cmake build has been revised somewhat.  By default the
 build builds just libdwarf and dwarfdump.
 
-cmake testing is not enabled (for now). The testing
-only tests various internal interfaces, not
-libdwarf interfaces.
+cmake testing is not enabled (for now). The testing only tests
+various internal interfaces, not libdwarf interfaces. Neither
+'make test' nor 'make install' have been verified to work
+properly with Makefiles created by cmake (27 April 2019)
 
-Lets assume the base directory of the the
-libdwarf package in a directory named code at /path/to/code
-     cmake /path/to/code
+Lets assume the base directory of the the libdwarf source in a
+directory named 'code' inside the directory '/path/to/' Always
+arrange to issue the cmake command in an empty directory.
+For example:
+
+    mkdir /tmp/bld
+    cd /tmp/bld
+    cmake /path/to/code
+    make
+
+
 Will build libdwarf (a static library, a libdwarf.a) and
-dwarfdump (linking to that static library).  And, in this
-case there is no libelf so dwarfdump won't read archives or
-honor requests to print elf headers.
+dwarfdump (linking to that static library).  If there is no
+libelf.h present during cmake/build then dwarfdump won't read
+archives or honor requests to print elf headers.
 
 To show all the available cmake options we'll show the
 default build,
 
 The default build is identical to
+
     cmake -Dlibelf:BOOL=yes \
         -Ddodwarfgen:BOOL=NO \
         -Ddodwarfexample:BOOL=NO \
@@ -33,19 +43,22 @@ The default build is identical to
     make
 
 The short form, doing the same as the default:
+
     cmake /path/to/code
     make
 
-The short form, nolibelf:
+The short form, nolibelf, for when you wish to build without
+libelf even if libelf.h and libelf are present:
+
     cmake -Dlibelf:BOOL=NO /path/to/code
+
 For this case any attempt to compile dwarfgen will be
 overridden: dwarfgen requires libelf.
 
 For dwarfexample:
+
     cmake -Ddodwarfexample:BOOL=YES /path/to/code
     make
 
 If libelf is missing -Ddodwarfgen:BOOL=YES will not be honored
 as dwarfgen will not build without libelf.
-
-    
