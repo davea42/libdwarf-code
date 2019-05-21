036ab6807e31173e151261d5dce43450b52411e4
David Anderson <davea42@linuxmail.org>
2019-05-21 11:09:26 -0700

    Adding a Windows-only #define which suppresses warnings. 	modified:   dwarfdump/dwarf_tsearchbal.c

--- dwarfdump/dwarf_tsearchbal.c
+++ dwarfdump/dwarf_tsearchbal.c
@@ -55,7 +55,9 @@ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 */
 
-
+#ifdef _WIN32
+#define _CRT_SECURE_NO_WARNINGS
+#endif /* _WIN32 */
 
 #include "config.h"
 #ifdef HAVE_UNUSED_ATTRIBUTE
