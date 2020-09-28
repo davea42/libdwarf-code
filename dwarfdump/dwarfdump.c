b4f4332f94425aa4f9250ccdf77389f3db3a3c43
David Anderson <davea42@linuxmail.org>
2020-09-28 07:25:28 -0700

    Very slight wording change in debuglink output. 	modified:   dwarfdump/dwarfdump.c
    Updated baseline for latest wording.
    	modified:   dwarfexample/debuglink.base

--- dwarfdump/dwarfdump.c
+++ dwarfdump/dwarfdump.c
@@ -2111,38 +2111,39 @@ print_gnu_debuglink(Dwarf_Debug dbg, Dwarf_Error *err)
                 &filesize, &errcode);
             if (res == DW_DLV_NO_ENTRY) {
                 if (glflags.verbose) {
-                  printf(" file does not exist\n");
+                    printf(" file above does not exist\n");
                 }
                 continue;
             }
             if (res == DW_DLV_ERROR) {
-                printf(" file access attempt lead to error %s\n",
+                printf("       access attempt of the above leads"
+                    " to error %s\n",
                     dwarf_errmsg_by_number(errcode));
                 continue;
             }
             switch(ftype) {
             case DW_FTYPE_ELF:
-                printf("       file exists and is an Elf object\n");
+                printf("       file above is an Elf object\n");
                 break;
             case DW_FTYPE_MACH_O:
-                printf("       file exists and is a Mach-O object\n");
+                printf("       file above is a Mach-O object\n");
                 break;
             case DW_FTYPE_PE:
-                printf("       file exists and is a PE object");
+                printf("       file above is a PE object");
                 break;
             case DW_FTYPE_CUSTOM_ELF:
-                printf("       file exists and is a custom elf object");
+                printf("       file above is a custom elf object");
                 break;
             case DW_FTYPE_ARCHIVE:
                 if (glflags.verbose) {
-                    printf("       file exists and is an archive "
-                    "so ignore it.");
+                    printf("       file above is an archive "
+                        "so ignore it.\n");
                 }
                 continue;
             default:
                 if (glflags.verbose) {
-                    printf("       file exists but is not an object "
-                        "type we recognize\n");
+                    printf("       file above is not"
+                        " any object type we recognize\n");
                 }
                 continue;
             }
