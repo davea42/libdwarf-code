0da06339dff7c9e4f98688d6afc84f2b5aaa0d0f
David Anderson <davea42@linuxmail.org>
2020-09-29 10:24:27 -0700

    dwarfexample: * getdebuglink.c: Now follows paths and compares   buildid of the executable vs the debug object.   Uses build-id to compare the objects.   The comment at the beginning of this source   explains how a current GNU pattern separated   debug is built.
    New baseline to match new output.
    	modified:   debuglink.base

--- dwarfexample/getdebuglink.c
+++ dwarfexample/getdebuglink.c
@@ -1,4 +1,4 @@
-/*  David Anderson. 2019. This small program is hereby
+/*  David Anderson.  2019-2020. This small program is hereby
     placed into the public domain to be copied or
     used by anyone for any purpose.
 
@@ -6,9 +6,33 @@
     https://sourceware.org/gdb/onlinedocs/\
         gdb/Separate-Debug-Files.html
     to calculate the crc of a file.
+    It's unclear whether one can legally
+    copy the crc code into this library/example
+    source, so we do nothing with the crc.
+
+    An emerging GNU pattern seems to be:
+        If a path found from debuglink or
+        build-id matches an elf object then
+        both the objects may bave a build-id
+        and if it is the right object the build-ids
+        will match.  This pattern does not
+        refer to the crc from the executable
+        debuglink.
+    To build a separate debug file x.debug with
+    DWARF and an executable with just debugid
+    and debuglink data using the pattern
+    seen in Ubuntu 20.04:
+
+    first compile and link, creating x
+    then:
+    objcopy --only-keep-debug x x.debug
+    objcopy --strip-debug x
+    objcopy --add-gnu-debuglink=x.debug x
+
+    See 'man objcopy' or
+    https://sourceware.org/binutils/docs/binutils/objcopy.html 
+    for more information.
 */
-
-
 
 #include "config.h"
 #include <stdio.h>
@@ -27,16 +51,67 @@
 #include "dwarf.h"
 #include "libdwarf.h"
 
-char trueoutpath[2000];
+#ifdef HAVE_UNUSED_ATTRIBUTE
+#define  UNUSEDARG __attribute__ ((unused))
+#else
+#define  UNUSEDARG
+#endif
 
+#define TRUE 1
+#define FALSE 1
+char trueoutpath[2000];
 static const char *dlname = ".gnu_debuglink";
 static const char *buildidname = ".note.gnu.buildid";
 
 static int doprintbuildid = 1;
 static int doprintdebuglink = 1;
 
-static void
-one_file_debuglink(char *path)
+static int
+blockmatch(unsigned char *l,
+    unsigned char* r,
+    unsigned length)
+{
+    unsigned int i = 0;
+    for( ; i < length; ++i) {
+        if (l[i] != r[i]) {
+            return FALSE;
+        }
+    }
+    return TRUE;
+}
+
+/*  The debug version we expect not to have debuglink,
+    checking here if buildid matches. */
+static int
+match_buildid(const char *prefix,
+    UNUSEDARG  unsigned char *crc_base,
+    unsigned         buildid_length_base,
+    unsigned  char  * buildid_base,
+    UNUSEDARG char *   path_base,
+
+    UNUSEDARG unsigned char  * crc_debug,
+    unsigned  buildid_length_debug,
+    unsigned  char *buildid_debug,
+    UNUSEDARG char *   path_debug)
+{
+    if(buildid_length_base != buildid_length_debug) {
+        return DW_DLV_NO_ENTRY;
+    }
+    if (!blockmatch(buildid_base,buildid_debug,
+        buildid_length_base)) {
+        return DW_DLV_NO_ENTRY;
+    }
+    printf("%s executable and debug buildid match\n",prefix);
+    return DW_DLV_OK;
+}
+
+static int
+one_file_debuglink_internal(const char *prefix,
+    char *path_in,
+    unsigned char * crc_in,
+    unsigned        buildid_len_in,
+    unsigned char * buildid_in,
+    char          * debug_path_in)
 {
     int res = 0;
     Dwarf_Debug dbg = 0;
@@ -51,8 +126,19 @@ one_file_debuglink(char *path)
     unsigned buildid_length = 0;
     char ** paths = 0; /* must be freed */
     unsigned paths_count = 0;
+    char  * path = 0;
+    char  * basepath = 0;
     Dwarf_Error error = 0;
 
+
+    path = basepath = path_in;
+
+    if (debug_path_in) {
+        path = debug_path_in;
+        printf("%s==Referred-path: %s\n",prefix,debug_path_in);
+    } else {
+        printf("%s==Exec-path    : %s\n",prefix,basepath);
+    }
     res = dwarf_init_path(path,
         trueoutpath,
         sizeof(trueoutpath),
@@ -62,15 +148,17 @@ one_file_debuglink(char *path)
         &dbg,
         0,0,0,&error);
     if (res == DW_DLV_ERROR) {
-        printf("Error from libdwarf opening \"%s\":  %s\n",
+        printf("%sError from libdwarf opening \"%s\":  %s\n",
+            prefix,
             path,
             dwarf_errmsg(error));
-        return;
+        return res;
     }
     if (res == DW_DLV_NO_ENTRY) {
-        printf("There is no such file as \"%s\"\n",
+        printf("%sThere is no such file as \"%s\"\n",
+            prefix,
             path);
-        return;
+        return DW_DLV_NO_ENTRY;
     }
     /*  We could call dwarf_add_debuglink_global_path()
         for each additional global path beside the default.
@@ -90,21 +178,22 @@ one_file_debuglink(char *path)
         &paths_count,
         &error);
     if (res == DW_DLV_ERROR) {
-        printf("Error from libdwarf accessing debuglink "
+        printf("%sError from libdwarf accessing debuglink "
             "related sections in \"%s\": %s\n",
+            prefix,
             path,
             dwarf_errmsg(error));
-        res = dwarf_finish(dbg,&error);
-        return;
+        dwarf_finish(dbg,&error);
+        return res;
     } else if (res == DW_DLV_NO_ENTRY) {
-        printf("There is no %s or %s section in \"%s\"\n",
-            dlname,buildidname,path);
+        printf("%sThere is no %s or %s section in \"%s\"\n",
+            prefix,dlname,buildidname,path);
         res = dwarf_finish(dbg,&error);
-        return;
+        return res;
     }
     if (doprintdebuglink && crc) {
-        printf(" Section %s\n",dlname);
-        printf(" Debuglink name  : %s",debuglinkpath);
+        printf("%s Section %s\n",prefix,dlname);
+        printf("%s Debuglink name  : %s",prefix,debuglinkpath);
         {
             unsigned char *crcx = 0;
             unsigned char *end = 0;
@@ -118,18 +207,17 @@ one_file_debuglink(char *path)
         }
         printf("\n");
         if (debuglinkfullpath_strlen) {
-            printf(" Debuglink target: %s\n",debuglinkfullpath);
+            printf("%s Debuglink target: %s\n",
+                prefix,debuglinkfullpath);
         }
-
     }
-
     if (doprintbuildid && buildid) {
-        printf(" Section %s\n",buildidname);
-        printf(" Build-id  type     : %u\n", buildid_type);
-        printf(" Build-id  ownername: %s\n",
+        printf("%s Section %s\n",prefix,buildidname);
+        printf("%s Build-id  type     : %u\n",prefix, buildid_type);
+        printf("%s Build-id  ownername: %s\n",prefix,
             buildidownername);
-        printf(" Build-id  length   : %u\n",buildid_length);
-        printf(" Build-id           : ");
+        printf("%s Build-id  length   : %u\n",prefix,buildid_length);
+        printf("%s Build-id           : ",prefix);
         {
             const unsigned char *cur = 0;
             const unsigned char *end = 0;
@@ -141,6 +229,19 @@ one_file_debuglink(char *path)
             }
         }
         printf("\n");
+    }
+    if (debug_path_in) {
+        res = match_buildid(prefix,
+            crc_in,buildid_len_in,buildid_in,
+            basepath,
+            /* following is the target, ie, debug */
+            crc,buildid_length,buildid,path);
+        if ( res == DW_DLV_OK) {
+            free(paths);
+            free(debuglinkfullpath);
+            dwarf_finish(dbg,&error);
+            return res;
+        }
     }
 
     /*  We could ignore the paths and paths_count
@@ -156,44 +257,54 @@ one_file_debuglink(char *path)
         Dwarf_Unsigned filesize = 0;
         int errcode = 0;
 
-        printf("Path [%2u] %s\n",i,pa);
-        /*  First, opn the file to determine if it exists.
+        printf("%s Path [%2u] %s\n",prefix,i,pa);
+        /*  First, open the file to determine if it exists.
             If not, loop again */
         res = dwarf_object_detector_path(pa,
             outpath,outpathlen,&ftype,&endian,&offsetsize,
             &filesize, &errcode);
         if (res == DW_DLV_NO_ENTRY) {
-            printf(" file above does not exist\n");
+            printf("%s file above does not exist\n",prefix);
             continue;
         }
         if (res == DW_DLV_ERROR) {
-            printf(" file above access attempt lead to error %s\n",
-                dwarf_errmsg_by_number(errcode));
+            printf("%s file above access attempt lead to error %s\n",
+                dwarf_errmsg_by_number(errcode),prefix);
             continue;
         }
         switch(ftype) {
         case DW_FTYPE_ELF:
-            printf(" file above is an Elf object\n");
+            printf("%s file above is an Elf object\n",prefix);
             break;
         case DW_FTYPE_MACH_O:
-            printf(" file above is a Mach-O object\n");
+            printf("%s file above is a Mach-O object\n",prefix);
             break;
         case DW_FTYPE_PE:
-            printf(" file above is a PE object");
+            printf("%s file above is a PE object",prefix);
             break;
 
         case DW_FTYPE_CUSTOM_ELF:
-            printf(" file above is a custom elf object");
+            printf("%s file above is a custom elf object",prefix);
             break;
         case DW_FTYPE_ARCHIVE:
-            printf(" file above is an archive so ignore it.");
+            printf("%s file above is an archive so ignore it.",
+                prefix);
             continue;
         default:
-            printf(" file above is not an object type"
-                " we recognize\n");
+            printf("%s file above is not an object type"
+                " we recognize\n",prefix);
             continue;
         }
-
+        /*  Now see if the debug has buildid matching
+            the executable. */
+        if (!debug_path_in) {
+            res = one_file_debuglink_internal(
+                "    ",
+                path,crc,buildid_length,
+                buildid,pa);
+        }
+        if (res == DW_DLV_OK) {
+        }
         /*  if crc is non-null calculate the crc of the
             opened file and compare the 4-byte values.
             If they match, this is the desired object file
@@ -204,11 +315,17 @@ one_file_debuglink(char *path)
     free(paths);
     free(debuglinkfullpath);
     dwarf_finish(dbg,&error);
-    return;
+    return DW_DLV_OK;
 }
 
+static void
+one_file_debuglink(char *path)
+{
+   one_file_debuglink_internal("",path,0,0,0,0);
+}
 
-int main(int argc, char **argv)
+int
+main(int argc, char **argv)
 {
     int i = 1;
     char *filenamein = 0;
