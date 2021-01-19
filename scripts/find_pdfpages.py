87567f57649c306cdbad3502c1ebbf6f7a5ed603
David Anderson <davea42@linuxmail.org>
2021-01-19 09:35:46 -0800

    Fixed indents 	modified:   find_pdfpages.py

--- scripts/find_pdfpages.py
+++ scripts/find_pdfpages.py
@@ -12,63 +12,63 @@ import os
 import sys
 
 def scanfile(path):
-  try:
-    fx = open(path,"r")
-  except IOError as  message:
-    print("File",path,"could not be opened: ", message)
-    sys.exit(1)
-  iline = 0
-  contentpage = 0
-  abstractpage = 0
-  lastline = ''
-  ipage = 0
-  while 1:
-    iline = int(iline)+1
     try:
-      rline = fx.readline()
-    except IOError as message:
-      print("read fails, line ",iline,message)
-      sys.exit(1)
-    if rline == '':
-      break
-    if rline[0] == chr(0xc):
-      ipage = int(ipage) + 1
-      wds = lastline.split()
-      if len(wds) < 3:
-         continue
-      if wds[0] != "-":
-         continue
-      if wds[2] != "-":
-         continue
-      pnum = wds[1]
-      if not pnum.isdigit():
-         continue
-      contentpage = ipage
-    else:
-      if len(rline) < 2:
-        pass
-      else:
-        lastline = rline.strip()
-  # Ends with lone formfeed.
-  abstractpage= int(ipage)
-  contentpage = int(contentpage) + 1
-  return (contentpage,abstractpage)
-if __name__ == '__main__':
-  i = 1
-  ct = 0
-  if i < len(sys.argv):
-     path = sys.argv[i]
-     #print("=======path=",path)
-     (a,c) =scanfile(path);
-     if int(a) == 0:
-       print("Could not find abstract page")
-       sys.exit(1)
-     if int(c) == 0:
-       print("Could not find abstract page")
-       sys.exit(1)
-     print(c)
-     print(a)
-  else:
-     print("file name argument(s) required");
-     sys.exit(1)
+        fx = open(path,"r")
+    except IOError as  message:
+        print("File",path,"could not be opened: ", message)
+        sys.exit(1)
+    iline = 0
+    contentpage = 0
+    abstractpage = 0
+    lastline = ''
+    ipage = 0
+    while 1:
+        iline = int(iline)+1
+        try:
+            rline = fx.readline()
+        except IOError as message:
+            print("read fails, line ",iline,message)
+            sys.exit(1)
+        if rline == '':
+            break
+        if rline[0] == chr(0xc):
+            ipage = int(ipage) + 1
+            wds = lastline.split()
+            if len(wds) < 3:
+                continue
+            if wds[0] != "-":
+                continue
+            if wds[2] != "-":
+                continue
+            pnum = wds[1]
+            if not pnum.isdigit():
+                continue
+            contentpage = ipage
+        else:
+            if len(rline) < 2:
+                pass
+            else:
+                lastline = rline.strip()
+    # Ends with lone formfeed.
+    abstractpage= int(ipage)
+    contentpage = int(contentpage) + 1
+    return (contentpage,abstractpage)
 
+if __name__ == '__main__':
+    i = 1
+    ct = 0
+    if i < len(sys.argv):
+        path = sys.argv[i]
+        #print("=======path=",path)
+        (a,c) =scanfile(path);
+        if int(a) == 0:
+            print("Could not find abstract page")
+            sys.exit(1)
+        if int(c) == 0:
+            print("Could not find abstract page")
+            sys.exit(1)
+        print(c)
+        print(a)
+    else:
+        print("file name argument(s) required");
+        sys.exit(1)
