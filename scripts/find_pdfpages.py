f23a8d71d9ee2f3023e4984a347c66678f78dc70
David Anderson <davea42@linuxmail.org>
2021-05-15 13:28:27 -0700

    Used the tool 'black' to reformat all these python3 files to standard python3 format. 	modified:   ChangeLog 	modified:   bugxml/bugrecord.py 	modified:   bugxml/readbugs.py 	modified:   scripts/ChangeLog 	modified:   scripts/conddef.py 	modified:   scripts/find_pdfpages.py 	modified:   scripts/funcfinderhdr.py 	modified:   scripts/funcfindermm.py 	modified:   scripts/funcfindersrcs.py 	modified:   scripts/haveinclude.py 	modified:   tsearch/ChangeLog 	new file:   tsearch/ChangeLog2020 	modified:   tsearch/scripts/comparator.py 	modified:   tsearch/scripts/concatlines.py

--- scripts/find_pdfpages.py
+++ scripts/find_pdfpages.py
@@ -1,4 +1,4 @@
-#/usr/bin/env python3
+# /usr/bin/env python3
 """
 Run this against the output of pdftotxt
 Here we use formfeeds to identify pages and page numbers.
@@ -12,27 +12,28 @@ See also scripts/rebuildpdf.sh
 import os
 import sys
 
+
 def scanfile(path):
     try:
-        fx = open(path,"r")
-    except IOError as  message:
-        print("File",path,"could not be opened: ", message)
+        fx = open(path, "r")
+    except IOError as message:
+        print("File", path, "could not be opened: ", message)
         sys.exit(1)
     iline = 0
     contentpage = 0
     abstractpage = 0
-    lastline = ''
+    lastline = ""
     ipage = 0
     while 1:
-        iline = int(iline)+1
+        iline = int(iline) + 1
         try:
             rline = fx.readline()
         except IOError as message:
-            print("read fails, line ",iline,message)
+            print("read fails, line ", iline, message)
             sys.exit(1)
-        if rline == '':
+        if rline == "":
             break
-        if rline[0] == chr(0xc):
+        if rline[0] == chr(0xC):
             ipage = int(ipage) + 1
             wds = lastline.split()
             if len(wds) < 3:
@@ -51,17 +52,18 @@ def scanfile(path):
             else:
                 lastline = rline.strip()
     # Ends with lone formfeed.
-    abstractpage= int(ipage)
+    abstractpage = int(ipage)
     contentpage = int(contentpage) + 1
-    return (contentpage,abstractpage)
+    return (contentpage, abstractpage)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     i = 1
     ct = 0
     if i < len(sys.argv):
         path = sys.argv[i]
-        #print("=======path=",path)
-        (a,c) =scanfile(path);
+        # print("=======path=",path)
+        (a, c) = scanfile(path)
         if int(a) == 0:
             print("Could not find abstract page")
             sys.exit(1)
@@ -71,5 +73,5 @@ if __name__ == '__main__':
         print(c)
         print(a)
     else:
-        print("file name argument(s) required");
+        print("file name argument(s) required")
         sys.exit(1)
