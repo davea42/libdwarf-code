11a4f671bc4ec41da75261063a5e96bc08e637a7
David Anderson <davea42@linuxmail.org>
2021-07-13 18:44:20 -0700

    All the ENUM_AND_BINARY_SEARCH ifdefs and code removed, none was used by libdwarf. We do not use the enum form of names.n and we use the switch() form in names.c, not binary search. 	modified:   src/lib/libdwarf/gennames.c

--- src/lib/libdwarf/gennames.c
+++ src/lib/libdwarf/gennames.c
@@ -46,7 +46,6 @@
 #include "libdwarf_private.h" /* for DW_VERSION_DATE_STR */
 #include "libdwarf_version.h" /* for DW_VERSION_DATE_STR */
 
-#undef ENUM_AND_BINARY_SEARCH
 
 /*  gennames.c
     Prints routines to return constant name for the associated value
@@ -126,10 +125,6 @@ static const unsigned prefix_root_len = 3;
 static FILE *f_dwarf_in;
 static FILE *f_names_h;
 static FILE *f_names_c;
-#ifdef ENUM_AND_BINARY_SEARCH 
-static FILE *f_names_enum_h;
-static FILE *f_names_new_h;
-#endif /* ENUM_AND_BINARY_SEARCH */
 
 /* Size unchecked, but large enough. */
 static char prefix[200] = "";
@@ -205,12 +200,6 @@ process_args(int argc, char *argv[])
             use_switch = TRUE;
             use_tables = FALSE;
             break;
-#ifdef ENUM_AND_BINARY_SEARCH
-        case 't':
-            use_switch = FALSE;
-            use_tables = TRUE;
-            break;
-#endif /* ENUM_AND_BINARY_SEARCH */
         default:
             usage_error = TRUE;
             break;
@@ -306,16 +295,8 @@ OpenAllFiles(void)
     const char *dwarf_h      = "dwarf.h";
     const char *names_h      = "dwarf_names.h";
     const char *names_c      = "dwarf_names.c";
-#ifdef ENUM_AND_BINARY_SEARCH
-    const char *names_enum_h = "dwarf_names_enum.h";
-    const char *names_new_h  = "dwarf_names_new.h";
-#endif /* ENUM_AND_BINARY_SEARCH */
 
     f_dwarf_in = open_path(input_name,dwarf_h,"r");
-#ifdef ENUM_AND_BINARY_SEARCH
-    f_names_enum_h = open_path(output_name,names_enum_h,"w");
-    f_names_new_h = open_path(output_name,names_new_h,"w");
-#endif /* ENUM_AND_BINARY_SEARCH */
     f_names_h = open_path(output_name,names_h,"w");
     f_names_c = open_path(output_name,names_c,"w");
 }
@@ -323,34 +304,6 @@ OpenAllFiles(void)
 static void
 GenerateInitialFileLines(void)
 {
-#ifdef ENUM_AND_BINARY_SEARCH
-    /* Generate entries for 'dwarf_names_enum.h' */
-    fprintf(f_names_enum_h,
-        "/* Automatically generated, do not edit. */\n");
-    fprintf(f_names_enum_h,"/* Generated sourcedate %s */\n",
-        DW_VERSION_DATE_STR);
-    fprintf(f_names_enum_h,"\n/* BEGIN FILE */\n\n");
-    fprintf(f_names_enum_h,"#ifndef __DWARF_NAMES_ENUM_H__\n");
-    fprintf(f_names_enum_h,"#define __DWARF_NAMES_ENUM_H__\n");
-
-    /* Generate entries for 'dwarf_names_new.h' */
-    fprintf(f_names_new_h,
-        "/* Automatically generated, do not edit. */\n");
-    fprintf(f_names_new_h,"/* Generated sourcedate %s */\n",
-        DW_VERSION_DATE_STR);
-    fprintf(f_names_new_h,"\n/* BEGIN FILE */\n\n");
-    fprintf(f_names_new_h,"/*  define DWARF_PRINT_PREFIX "
-        "before this\n");
-    fprintf(f_names_new_h,"    point if you wish to.  */\n");
-    fprintf(f_names_new_h,"#ifndef DWARF_PRINT_PREFIX\n");
-    fprintf(f_names_new_h,"#define DWARF_PRINT_PREFIX dwarf_\n");
-    fprintf(f_names_new_h,"#endif\n");
-    fprintf(f_names_new_h,"#define dw_glue(x,y) x##y\n");
-    fprintf(f_names_new_h,"#define dw_glue2(x,y) dw_glue(x,y)\n");
-    fprintf(f_names_new_h,"#define DWPREFIX(x) "
-        "dw_glue2(DWARF_PRINT_PREFIX,x)\n");
-#endif /* ENUM_AND_BINARY_SEARCH */
-
     /* Generate entries for 'dwarf_names.h' */
     fprintf(f_names_h,"/* Generated routines, do not edit. */\n");
     fprintf(f_names_h,"/* Generated sourcedate %s */\n",
@@ -370,70 +323,12 @@ GenerateInitialFileLines(void)
     fprintf(f_names_c,"\n/* BEGIN FILE */\n\n");
     fprintf(f_names_c,"#include \"dwarf.h\"\n\n");
     fprintf(f_names_c,"#include \"libdwarf.h\"\n\n");
-
-#ifdef ENUM_AND_BINARY_SEARCH
-    if (use_tables) {
-        fprintf(f_names_c,"typedef struct Names_Data {\n");
-        fprintf(f_names_c,"    const char *l_name; \n");
-        fprintf(f_names_c,"    unsigned    value;  \n");
-        fprintf(f_names_c,"} Names_Data;\n\n");
-
-        /* Generate code to find an entry */
-        fprintf(f_names_c,
-            "/* Use standard binary search to get entry */\n");
-        fprintf(f_names_c,"static int\nfind_entry(Names_Data *table,"
-            "const int last,unsigned value, const char **s_out)\n");
-        fprintf(f_names_c,"{\n");
-        fprintf(f_names_c,"    int low = 0;\n");
-        fprintf(f_names_c,"    int high = last;\n");
-        fprintf(f_names_c,"    int mid;\n");
-        fprintf(f_names_c,"    unsigned maxval = "
-            "table[last-1].value;\n");
-        fprintf(f_names_c,"\n");
-        fprintf(f_names_c,"    if (value > maxval) {\n");
-        fprintf(f_names_c,"        return DW_DLV_NO_ENTRY;\n");
-        fprintf(f_names_c,"    }\n");
-        fprintf(f_names_c,"    while (low < high) {\n");
-        fprintf(f_names_c,"        mid = low + ((high - low) "
-            "/ 2);\n");
-        fprintf(f_names_c,"        if (mid == last) {\n");
-        fprintf(f_names_c,"            break;\n");
-        fprintf(f_names_c,"        }\n");
-        fprintf(f_names_c,"        if (table[mid].value "
-            "< value) {\n");
-        fprintf(f_names_c,"            low = mid + 1;\n");
-        fprintf(f_names_c,"        }\n");
-        fprintf(f_names_c,"        else {\n");
-        fprintf(f_names_c,"              high = mid;\n");
-        fprintf(f_names_c,"        }\n");
-        fprintf(f_names_c,"    }\n");
-        fprintf(f_names_c,"\n");
-        fprintf(f_names_c,"    if (low < last && table[low]."
-            "value == value) {\n");
-        fprintf(f_names_c,"        /* Found: low is the entry */\n");
-        fprintf(f_names_c,"      *s_out = table[low].l_name;\n");
-        fprintf(f_names_c,"      return DW_DLV_OK;\n");
-        fprintf(f_names_c,"    }\n");
-        fprintf(f_names_c,"    return DW_DLV_NO_ENTRY;\n");
-        fprintf(f_names_c,"}\n");
-        fprintf(f_names_c,"\n");
-    }
-#endif /* ENUM_AND_BINARY_SEARCH */
 }
 
 /* Close files and write basic trailers */
 static void
 WriteFileTrailers(void)
 {
-#ifdef ENUM_AND_BINARY_SEARCH
-    /* Generate entries for 'dwarf_names_enum.h' */
-    fprintf(f_names_enum_h,"#endif /* __DWARF_NAMES_ENUM_H__ */\n");
-    fprintf(f_names_enum_h,"\n/* END FILE */\n");
-
-    /* Generate entries for 'dwarf_names_new.h' */
-    fprintf(f_names_new_h,"\n/* END FILE */\n");
-#endif /* ENUM_AND_BINARY_SEARCH */
-
     /* Generate entries for 'dwarf_names.h' */
 
     fprintf(f_names_h,"\n#ifdef __cplusplus\n");
@@ -450,10 +345,6 @@ static void
 CloseAllFiles(void)
 {
     fclose(f_dwarf_in);
-#ifdef ENUM_AND_BINARY_SEARCH
-    fclose(f_names_enum_h);
-    fclose(f_names_new_h);
-#endif /* ENUM_AND_BINARY_SEARCH */
     fclose(f_names_h);
     fclose(f_names_c);
 }
@@ -484,14 +375,6 @@ GenerateOneSet(void)
     printf("\nList after sorting:\n");
     PrintArray();
 #endif /* TRACE_ARRAY */
-#ifdef ENUM_AND_BINARY_SEARCH
-    /* Generate entries for 'dwarf_names_enum.h' */
-    fprintf(f_names_enum_h,"\nenum Dwarf_%s_e {\n",prefix_id);
-
-    /* Generate entries for 'dwarf_names_new.h' */
-    fprintf(f_names_new_h,"int DWPREFIX(get_%s_name) "
-        "(unsigned int, const char **);\n",prefix_id);
-#endif /* ENUM_AND_BINARY_SEARCH */
 
     /* Generate entries for 'dwarf_names.h' and libdwarf.h */
     fprintf(f_names_h,"extern int dwarf_get_%s_name"
@@ -505,15 +388,7 @@ GenerateOneSet(void)
         prefix_id);
     fprintf(f_names_c,"    const char ** s_out)\n");
     fprintf(f_names_c,"{\n");
-
-    if (use_tables) {
-#ifdef ENUM_AND_BINARY_SEARCH
-        fprintf(f_names_c,"    static Names_Data Dwarf_%s_n[] = {\n",
-            prefix_id);
-#endif /* ENUM_AND_BINARY_SEARCH */
-    } else {
-        fprintf(f_names_c,"    switch (val) {\n");
-    }
+    fprintf(f_names_c,"    switch (val) {\n");
 
     for (u = 0; u < array_count; ++u) {
         /* Check if value already dumped */
@@ -529,59 +404,19 @@ GenerateOneSet(void)
         }
         prev_value = group_array[u].value;
 
-#ifdef ENUM_AND_BINARY_SEARCH
-        /*  Generate entries for 'dwarf_names_enum.h'.
-            The 39 just makes nice formatting in the output. */
-        len = 39 - strlen(prefix);
-        fprintf(f_names_enum_h,"    %s_%-*s = 0x%04x",
-            prefix,(int)len,group_array[u].name,group_array[u].value);
-        fprintf(f_names_enum_h,(u + 1 < array_count) ? ",\n" : "\n");
-#endif /* ENUM_AND_BINARY_SEARCH */
-
         /* Generate entries for 'dwarf_names.c' */
-        if (use_tables) {
-#ifdef ENUM_AND_BINARY_SEARCH
-            fprintf(f_names_c,"    {/* %3u */ \"%s_%s\", ",
-                actual_array_count,prefix,group_array[u].name);
-            fprintf(f_names_c," %s_%s}", prefix,group_array[u].name);
-            fprintf(f_names_c,(u + 1 < array_count) ? ",\n" : "\n");
-#endif /* ENUM_AND_BINARY_SEARCH */
-        } else {
-            fprintf(f_names_c,"    case %s_%s:\n",
-                prefix,group_array[u].name);
-            fprintf(f_names_c,"        *s_out = \"%s_%s\";\n",
-                prefix,group_array[u].name);
-            fprintf(f_names_c,"        return DW_DLV_OK;\n");
-        }
+        fprintf(f_names_c,"    case %s_%s:\n",
+            prefix,group_array[u].name);
+        fprintf(f_names_c,"        *s_out = \"%s_%s\";\n",
+            prefix,group_array[u].name);
+        fprintf(f_names_c,"        return DW_DLV_OK;\n");
         ++actual_array_count;
     }
 
     /* Closing entries for 'dwarf_names_enum.h' */
-#ifdef ENUM_AND_BINARY_SEARCH
-    fprintf(f_names_enum_h,"};\n");
-#endif /* ENUM_AND_BINARY_SEARCH */
-
-    if (use_tables) {
-#ifdef ENUM_AND_BINARY_SEARCH
-        /* Closing entries for 'dwarf_names.c' */
-        fprintf(f_names_c,"    };\n\n");
-
-        /* Closing code for 'dwarf_names.c' */
-        fprintf(f_names_c,"    const int last_entry = %d;\n",
-            actual_array_count);
-        fprintf(f_names_c,"    /* find the entry */\n");
-        fprintf(f_names_c,
-            "    int r = find_entry(Dwarf_%s_n,"
-            "last_entry,val,s_out);\n",prefix_id);
-        fprintf(f_names_c,"    return r;\n");
-        fprintf(f_names_c,"}\n");
-#endif /* ENUM_AND_BINARY_SEARCH */
-    } else {
-        fprintf(f_names_c,"    }\n");
-        fprintf(f_names_c,"    return DW_DLV_NO_ENTRY;\n");
-        fprintf(f_names_c,"}\n");
-    }
-
+    fprintf(f_names_c,"    }\n");
+    fprintf(f_names_c,"    return DW_DLV_NO_ENTRY;\n");
+    fprintf(f_names_c,"}\n");
     /* Mark the group_array as empty */
     array_count = 0;
 }
