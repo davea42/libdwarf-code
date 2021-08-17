302d672243a0b9a8af8dd1982829d231d18f4a2f
David Anderson <davea42@linuxmail.org>
2021-08-17 09:03:37 -0700

    DO NOT USE THIS COMMIT.  revising interfaces and reformatting lines in progress. 	modified:   dd_regex.c 	modified:   dd_regex.h

--- src/bin/dwarfdump/dd_regex.h
+++ src/bin/dwarfdump/dd_regex.h
@@ -1,9 +1,24 @@
-#ifndef REGEX_H
-#define REGEX_H
+/*
+    regex - Regular expression pattern matching  and replacement
+  
+    By:  Ozan S. Yigit (oz)
+        Dept. of Computer Science
+        York University
+  
+    These routines are the PUBLIC DOMAIN equivalents of regex
+    routines as found in 4.nBSD UN*X, with minor extensions.
+  
+    These routines are derived from various implementations found
+    in software tools books, and Conroy's grep. They are NOT derived
+    from licensed/restricted software.
 
-extern char *re_comp(char *);
-extern int re_exec(char *);
-extern void re_modw(char *);
-extern int re_subs(char *, char *);
+    We are skipping the replacement function, which
+    dwarfdump does not require.
+*/
+#ifndef DD_REGEX_H
+#define DD_REGEX_H
 
-#endif /* REGEX_H */
+char *dd_re_comp(char *);
+int   dd_re_exec(char *);
+
+#endif /* DD_REGEX_H */
