ab88ad8cb76ab4e6ab21f30b50771bcbb7ed14e0
David Anderson <davea42@linuxmail.org>
2021-10-18 11:45:55 -0700

    If uintptr_t is defined in stdint.h at configure time we need to include stdint.h here for uintptr_t, which was done correctly everywhere else for uintptr_t. 	modified:   dwarf_pro_dnames.c

--- src/lib/libdwarfp/dwarf_pro_dnames.c
+++ src/lib/libdwarfp/dwarf_pro_dnames.c
@@ -32,6 +32,9 @@
 #ifdef HAVE_STRING_H
 #include <string.h>
 #endif /* HAVE_STRING_H */
+#ifdef HAVE_STDINT_H
+#include <stdint.h> /* uintptr_t, else configure generates.*/
+#endif /* HAVE_STDINT_H */
 #ifdef HAVE_STDLIB_H
 #include <stdlib.h> /* for exit(), C89 malloc */
 #endif /* HAVE_STDLIB_H */
