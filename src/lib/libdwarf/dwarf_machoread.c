9770b2cc4812857022a8e147bdfe1f102adba382
David Anderson <davea42@linuxmail.org>
2021-11-08 12:58:21 -0800

    Polishing the records on oss-fuzz tests. 	modified:   bugxml/data.txt
    Copyright accidentally slighly mangled. Fixed.
    	modified:   src/lib/libdwarf/dwarf_machoread.c

--- src/lib/libdwarf/dwarf_machoread.c
+++ src/lib/libdwarf/dwarf_machoread.c
@@ -1,7 +1,5 @@
 /*
-Copyright (c) 2019, David Anderson
-All rights reserved.
-cc
+Copyright (c) 2019, David Anderson All rights reserved.
 Redistribution and use in source and binary forms, with
 or without modification, are permitted provided that the
 following conditions are met:
