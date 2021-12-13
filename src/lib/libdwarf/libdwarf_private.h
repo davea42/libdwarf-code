801575b4b7d7ece387a8294e59c4458254f4f2de
David Anderson <davea42@linuxmail.org>
2021-12-13 08:44:34 -0800

    Alter the macro definitions so that the arguments have () at the point of use in the macro. A well known best-practice. 	modified:   libdwarf_private.h

--- src/lib/libdwarf/libdwarf_private.h
+++ src/lib/libdwarf/libdwarf_private.h
@@ -45,14 +45,14 @@
 #define ASNAR(func,t,s)                         \
     do {                                        \
         unsigned tbyte = sizeof(t) - sizeof(s); \
-        t = 0;                                  \
-        func(((char *)&t)+tbyte ,&s[0],sizeof(s));  \
+        (t) = 0;                                \
+        (func)(((char *)&t)+tbyte ,&s[0],sizeof(s));  \
     } while (0)
 #else /* LITTLE ENDIAN */
 #define ASNAR(func,t,s)                         \
     do {                                        \
-        t = 0;                                  \
-        func(&t,&s[0],sizeof(s));               \
+        (t) = 0;                                \
+        (func)(&(t),&(s)[0],sizeof(s));           \
     } while (0)
 #endif /* end LITTLE- BIG-ENDIAN */
 
@@ -64,45 +64,46 @@
     libdwarfp because of _dwarf_error()
     here.  */
 #ifdef WORDS_BIGENDIAN
-#define ASNARL(t,s,l)                   \
-    do {                                    \
-        unsigned tbyte = sizeof(t) - l;     \
-        if (sizeof(t) < l) {                \
+#define ASNARL(t,s,l)                     \
+    do {                                  \
+        unsigned tbyte = sizeof(t) - (l); \
+        if (sizeof(t) < (l)) {            \
             _dwarf_error(dbg,error,DW_DLE_XU_HASH_INDEX_ERROR); \
-            return DW_DLV_ERROR;             \
-        }                                   \
-        t = 0;                              \
-        dbg->de_copy_word(((char *)&t)+tbyte ,&s[0],l);\
+            return DW_DLV_ERROR;          \
+        }                                 \
+        (t) = 0;                          \
+        dbg->de_copy_word(((char *)&(t))+tbyte ,&(s)[0],(l));\
     } while (0)
 #else /* LITTLE ENDIAN */
 #define ASNARL(t,s,l)                 \
-    do {                                \
-        t = 0;                          \
-        if (sizeof(t) < l) {            \
+    do {                              \
+        (t) = 0;                      \
+        if (sizeof(t) < (l)) {        \
             _dwarf_error(dbg,error,DW_DLE_XU_HASH_INDEX_ERROR); \
-            return DW_DLV_ERROR;         \
-        }                               \
-        dbg->de_copy_word(&t,&s[0],l);  \
+            return DW_DLV_ERROR;      \
+        }                             \
+        dbg->de_copy_word(&(t),&(s)[0],(l));  \
     } while (0)
 #endif /* end LITTLE- BIG-ENDIAN */
 
 #ifdef WORDS_BIGENDIAN
-#define SIGN_EXTEND(dest, length)                                 \
-    do {                                                          \
-        if (*(Dwarf_Sbyte *)((char *)&dest +                      \
-            sizeof(dest) - length) < 0) {                         \
-            memcpy((char *)&dest, "\xff\xff\xff\xff\xff\xff\xff\xff",\
-                sizeof(dest) - length);                           \
-        }                                                         \
+#define SIGN_EXTEND(dest, length)                       \
+    do {                                                \
+        if (*(Dwarf_Sbyte *)((char *)&(dest) +          \
+            sizeof(dest) - (length)) < 0) {             \
+            memcpy((char *)&(dest),                     \
+                "\xff\xff\xff\xff\xff\xff\xff\xff",     \
+                sizeof(dest) - (length));               \
+        }                                               \
     } while (0)
 #else /* LITTLE ENDIAN */
-#define SIGN_EXTEND(dest, length)                               \
-    do {                                                        \
-        if (*(Dwarf_Sbyte *)((char *)&dest + (length-1)) < 0) { \
-            memcpy((char *)&dest+length,                        \
-                "\xff\xff\xff\xff\xff\xff\xff\xff",             \
-                sizeof(dest) - length);                         \
-        }                                                       \
+#define SIGN_EXTEND(dest, length)                                 \
+    do {                                                          \
+        if (*(Dwarf_Sbyte *)((char *)&(dest)+ ((length)-1)) < 0) { \
+            memcpy((char *)&(dest)+(length),                      \
+                "\xff\xff\xff\xff\xff\xff\xff\xff",               \
+                sizeof(dest) - (length));                         \
+        }                                                         \
     } while (0)
 #endif
 #endif /* LIBDWARF_PRIVATE_H */
