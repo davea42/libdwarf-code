d56bd7a112bdec8f5e969743eb20f81bfcbcf2f3
David Anderson <davea42@linuxmail.org>
2022-01-28 14:53:14 -0800

    Trivial include change. 	modified:   ../tag_tree/tag_common.c

--- src/bin/tag_tree/tag_common.c
+++ src/bin/tag_tree/tag_common.c
@@ -35,7 +35,6 @@ Portions Copyright (C) 2009-2012 David Anderson. All Rights Reserved.
 #include <string.h>
 #include <errno.h>/* For errno declaration. */
 #include <ctype.h>    /*  For isspace() declaration */
-
 #include "dwarf.h"
 #include "libdwarf.h"
 #include "dd_globals.h"
