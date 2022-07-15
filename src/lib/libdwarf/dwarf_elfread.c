78005a95d916c84b92f0ca58f75ded3a2e3f91d8
David Anderson <davea42@linuxmail.org>
2022-07-15 09:56:51 -0700

    Implemented a better, faster, universal way to deal with R_<machine>_NONE relocation records. Should have done it this way years ago. 	modified:   lib/libdwarf/dwarf_elfread.c

--- src/lib/libdwarf/dwarf_elfread.c
+++ src/lib/libdwarf/dwarf_elfread.c
@@ -346,11 +346,11 @@ update_entry(Dwarf_Debug dbg,
         reloc_size = 4;
     } else if (_dwarf_is_64bit_abs_reloc(type, machine)) {
         reloc_size = 8;
-    } else if (machine == EM_X86_64 && type == R_X86_64_NONE) {
-        /*  There is nothing to do. */
-        return DW_DLV_OK;
-    } else if (machine == EM_AARCH64 && type == R_AARCH64_NONE) {
-        /*  There is nothing to do. */
+    } else if (!type) {
+        /*  There is nothing to do. , this is the case such as
+            R_AARCH64_NONE and R_X86_64_NONE and the other machine
+            cases have it too. Most object files do not have
+            any relocation records of type R_<machine>_NONE.  */
         return DW_DLV_OK;
     } else {
         *error = DW_DLE_RELOC_SECTION_RELOC_TARGET_SIZE_UNKNOWN;
