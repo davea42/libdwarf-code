e9fbfc0fac2f8fdeb2dbf057d47acf6a4b9b6b51
David Anderson <davea42@linuxmail.org>
2023-02-20 10:21:43 -0800

    tools/updatesemanticversion.py 0.6.1 setting a new version. 	modified:   CMakeLists.txt 	modified:   configure.ac 	modified:   doc/libdwarf.dox 	modified:   doc/libdwarfp.mm 	modified:   meson.build 	modified:   src/lib/libdwarf/libdwarf.h

--- src/lib/libdwarf/libdwarf.h
+++ src/lib/libdwarf/libdwarf.h
@@ -99,10 +99,10 @@ extern "C" {
 */
 
 /* Semantic Version identity for this libdwarf.h */
-#define DW_LIBDWARF_VERSION "0.6.0"
+#define DW_LIBDWARF_VERSION "0.6.1"
 #define DW_LIBDWARF_VERSION_MAJOR 0
 #define DW_LIBDWARF_VERSION_MINOR 6
-#define DW_LIBDWARF_VERSION_MICRO 0
+#define DW_LIBDWARF_VERSION_MICRO 1
 
 #define DW_PATHSOURCE_unspecified 0
 #define DW_PATHSOURCE_basic     1
