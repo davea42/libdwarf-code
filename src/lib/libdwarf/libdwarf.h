407cd23ae8b129df6a979fc908eb8754568bf7b1
spring <sspring@163.com>
2023-06-14 20:40:24 +0800

    enable static/shared build on mingw gcc

--- src/lib/libdwarf/libdwarf.h
+++ src/lib/libdwarf/libdwarf.h
@@ -51,10 +51,14 @@
 #endif /* DW_API */
 
 #if defined(_WIN32) || defined(__CYGWIN__)
+#ifdef _MSC_VER
 # ifdef LIBDWARF_BUILD
 #  define DW_API __declspec(dllexport)
 # else /* !LIBDWARF_BUILD */
 #  define DW_API __declspec(dllimport)
+# endif
+#else
+# define DW_API
 # endif /* LIBDWARF_BUILD */
 #elif (defined(__SUNPRO_C)  || defined(__SUNPRO_CC))
 # if defined(PIC) || defined(__PIC__)
