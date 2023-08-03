104dd5f58359a13019c023fa7ec04706d9c9db24
flagarde <flagarde.programs@pm.me>
2023-08-03 17:14:20 +0800

    CMake

--- cmake/FindZSTD.cmake
+++ cmake/FindZSTD.cmake
@@ -48,16 +48,11 @@ endif ()
 find_library(ZSTD_LIBRARIES NAMES zstd)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(ZSTD
-        REQUIRED_VARS ZSTD_LIBRARIES ZSTD_INCLUDE_DIR ZSTD_VERSION)
+find_package_handle_standard_args(ZSTD REQUIRED_VARS ZSTD_LIBRARIES ZSTD_INCLUDE_DIR ZSTD_VERSION VERSION_VAR ZSTD_VERSION)
 
 if (ZSTD_FOUND)
-    set(ZSTD_INCLUDE_DIRS "${ZSTD_INCLUDE_DIR}")
-
     if (NOT TARGET ZSTD::ZSTD)
         add_library(ZSTD::ZSTD UNKNOWN IMPORTED)
-        set_target_properties(ZSTD::ZSTD PROPERTIES
-                IMPORTED_LOCATION "${ZSTD_LIBRARIES}"
-                INTERFACE_INCLUDE_DIRECTORIES "${ZSTD_INCLUDE_DIRS}")
+        set_target_properties(ZSTD::ZSTD PROPERTIES IMPORTED_LOCATION "${ZSTD_LIBRARIES}" INTERFACE_INCLUDE_DIRECTORIES "${ZSTD_INCLUDE_DIRS}")
     endif ()
 endif ()
