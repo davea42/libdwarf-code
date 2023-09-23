2867fa3cc7a79ba89c3b7f5672b6096ac4fc28ff
David Anderson <davea42@linuxmail.org>
2023-09-23 05:44:38 -0700

    New ossfuzz bug DW202309-001 recorded here now. 	modified:   bugxml/data.txt 	modified:   bugxml/dwarfbug.html 	modified:   bugxml/dwarfbug.xml 	modified:   bugxml/dwarfbuglohi.html
    Version now 0.8.1
    	modified:   CMakeLists.txt
    	modified:   configure.ac
    	modified:   doc/libdwarf.dox
    	modified:   doc/libdwarf.pdf
    	modified:   doc/libdwarfp.mm
    	modified:   meson.build
    	modified:   src/lib/libdwarf/libdwarf.h

--- src/lib/libdwarf/libdwarf.h
+++ src/lib/libdwarf/libdwarf.h
@@ -99,10 +99,10 @@ extern "C" {
 */
 
 /* Semantic Version identity for this libdwarf.h */
-#define DW_LIBDWARF_VERSION "0.8.0"
+#define DW_LIBDWARF_VERSION "0.8.1"
 #define DW_LIBDWARF_VERSION_MAJOR 0
 #define DW_LIBDWARF_VERSION_MINOR 8
-#define DW_LIBDWARF_VERSION_MICRO 0
+#define DW_LIBDWARF_VERSION_MICRO 1
 
 #define DW_PATHSOURCE_unspecified 0
 #define DW_PATHSOURCE_basic     1
