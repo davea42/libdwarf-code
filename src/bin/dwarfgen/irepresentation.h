6c761f6df684a5ae3afc864b557cc7f801fd0b5e
David Anderson <davea42@linuxmail.org>
2024-04-18 08:47:41 -0700

    Changes to follow the project codingstyle. No logic changes. 	modified:   src/bin/dwarfgen/dg_getopt.cc 	modified:   src/bin/dwarfgen/dwarfgen.cc 	modified:   src/bin/dwarfgen/ireppubnames.h 	modified:   src/bin/dwarfgen/irepresentation.h 	modified:   src/bin/dwarfgen/ireptodbg.h 	modified:   src/bin/dwarfgen/strtabdata.h

--- src/bin/dwarfgen/irepresentation.h
+++ src/bin/dwarfgen/irepresentation.h
@@ -1,27 +1,36 @@
 /*
-  Copyright (C) 2010-2016 David Anderson.  All rights reserved.
+Copyright (C) 2010-2016 David Anderson.  All rights reserved.
 
-  Redistribution and use in source and binary forms, with or without
-  modification, are permitted provided that the following conditions are met:
-  * Redistributions of source code must retain the above copyright
+Redistribution and use in source and binary forms, with
+or without modification, are permitted provided that the
+following conditions are met:
+
+*   Redistributions of source code must retain the above copyright
     notice, this list of conditions and the following disclaimer.
-  * Redistributions in binary form must reproduce the above copyright
-    notice, this list of conditions and the following disclaimer in the
-    documentation and/or other materials provided with the distribution.
-  * Neither the name of the example nor the
-    names of its contributors may be used to endorse or promote products
-    derived from this software without specific prior written permission.
+*   Redistributions in binary form must reproduce the above
+    copyright notice, this list of conditions and the
+    following disclaimer in the
+    documentation and/or other materials provided
+    with the distribution.
+*   Neither the name of the example nor the
+    names of its contributors may be used to endorse
+    or promote products
+    derived from this software without specific prior
+    written permission.
 
-  THIS SOFTWARE IS PROVIDED BY David Anderson ''AS IS'' AND ANY
-  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  DISCLAIMED. IN NO EVENT SHALL David Anderson BE LIABLE FOR ANY
-  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+THIS SOFTWARE IS PROVIDED BY David Anderson ''AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+NO EVENT SHALL David Anderson BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 */
 
@@ -52,7 +61,8 @@ class ElfSymbol {
 public:
     ElfSymbol():symbolValue_(0),
         nameIndex_(0) {};
-    ElfSymbol(Dwarf_Unsigned val, const std::string&name, strtabdata&stab):
+    ElfSymbol(Dwarf_Unsigned val, const std::string&name,
+        strtabdata&stab):
         symbolValue_(val),name_(name) {
         nameIndex_ = stab.addString(name);
     };
@@ -71,7 +81,8 @@ private:
 //  and gives those a recognizable type.
 //  As of 2023 the ElfSectIndex is an index into
 //  <vector>SectionForDwarf dwsectab
-//  whereas the libdwarfp arrays are fixed values (see NUM_DEBUG_SECTIONS)
+//  whereas the libdwarfp arrays are fixed values
+//  (see NUM_DEBUG_SECTIONS)
 class ElfSectIndex {
 public:
     ElfSectIndex():elfsect_(0) {};
@@ -82,7 +93,6 @@ public:
 private:
     unsigned elfsect_;
 };
-
 
 //  It's very easy to confuse the symbol number in an elf file
 //  with a symbol number in dwarfgen.
@@ -125,7 +135,9 @@ public:
     ElfSymbol &  getElfSymbol(ElfSymIndex symi) {
         size_t i = symi.getSymIndex();
         if (i >= elfSymbols_.size()) {
-            std::cerr << "Error, sym index " << i << "  to big for symtab size " << elfSymbols_.size() << std::endl;
+            std::cerr << "Error, sym index " << i <<
+            "  too big for symtab size " <<
+            elfSymbols_.size() << std::endl;
             exit(1);
         }
         return elfSymbols_[i];
@@ -135,7 +147,6 @@ private:
     strtabdata symstrtab_;
     ElfSymIndex  baseTextAddressSymbol_;
 };
-
 
 class IRepresentation {
 public:
