120f8c4fb9b459e7ad2917a6e44a001f502c6a64
David Anderson <davea42@linuxmail.org>
2024-09-18 09:56:50 -0700

    New standard tables for af table data. 	new file:   dwarfdump-af-table-ext.h 	new file:   dwarfdump-af-table-std.h

---
+++ src/bin/dwarfdump/dwarfdump-af-table-ext.h
@@ -0,0 +1,19 @@
+/* Generated table, do not edit. */
+/* Generated for source version 0.11.1 */
+
+#ifndef DWARFDUMP_AF_TABLE_STD_H
+#define DWARFDUMP_AF_TABLE_STD_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+struct Three_Key_Entry_s * dd_threekey_af_table_std[] = {
+{0,0,0,0,0,0}
+};
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* DWARFDUMP_AF_TABLE_STD H */
---
+++ src/bin/dwarfdump/dwarfdump-af-table-std.h
@@ -0,0 +1,18 @@
+/* Generated table, do not edit. */
+/* Generated for source version 0.11.1 */
+
+#ifndef DWARFDUMP_AF_TABLE_EXT_H
+#define DWARFDUMP_AF_TABLE_EXT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+struct Three_Key_Entry_s * dd_threekey_af_table_ext[] = {
+{0,0,0,0,0,0}
+};
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* DWARFDUMP_AF_TABLE_EXT H */
