754d5627c2d2d6f48a7e444fa5173ef447ae0c07
David Anderson <davea42@linuxmail.org>
2024-11-03 16:52:38 -0800

    copyright 	modified:   src/lib/libdwarf/libdwarf.h

--- src/lib/libdwarf/libdwarf.h
+++ src/lib/libdwarf/libdwarf.h
@@ -1,7 +1,7 @@
 /*
   Copyright (C) 2000-2010 Silicon Graphics, Inc.  All Rights Reserved.
   Portions Copyright 2007-2010 Sun Microsystems, Inc. All rights reserved.
-  Portions Copyright 2008-2023 David Anderson. All rights reserved.
+  Portions Copyright 2008-2024 David Anderson. All rights reserved.
   Portions Copyright 2008-2010 Arxan Technologies, Inc. All rights reserved.
   Portions Copyright 2010-2012 SN Systems Ltd. All rights reserved.
 
