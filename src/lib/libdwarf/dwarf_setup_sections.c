e02caf3da1b31c257bc9938a7d207b3c13b7cf0b
David Anderson <davea42@linuxmail.org>
2025-02-27 09:37:37 -0800

    for dwarf_setup_sections TESTING avoid doing getenv or referring to the alloc type. 	modified:   src/lib/libdwarf/dwarf_setup_sections.c Ensure that -DTESTING is set for dwarf_setup_sections testing. 	modified:   test/CMakeLists.txt 	modified:   test/Makefile.am

--- src/lib/libdwarf/dwarf_setup_sections.c
+++ src/lib/libdwarf/dwarf_setup_sections.c
@@ -95,7 +95,9 @@ add_debug_section_info(Dwarf_Debug dbg,
         secdata->dss_mmap_realarea = 0;
         secdata->dss_was_malloc = 0;
         /*  Just gets current global pref */
+#ifndef  TESTING
         secdata->dss_load_preference = dwarf_set_load_preference(0);
+#endif  /* TESTING */
         /* We don't yet know about SHF_COMPRESSED */
         debug_section->ds_duperr = duperr;
         debug_section->ds_emptyerr = emptyerr;
