c735260d0a3c43d92195b1a66bcffdfcfeb0c2ce
Alex Petenchea <alex.petenchea@gmail.com>
2025-12-14 22:21:46 +0800

    Prevent a NULL deref in _dwarf_loclists_fill_in_lle_head (#312)
    * Prevent a NULL deref in _dwarf_loclists_fill_in_lle_head() when thre is no loclists context
    
    * Use project's code style

--- src/lib/libdwarf/dwarf_loclists.c
+++ src/lib/libdwarf/dwarf_loclists.c
@@ -1287,6 +1287,12 @@ _dwarf_loclists_fill_in_lle_head(Dwarf_Debug dbg,
         if (res == DW_DLV_OK) {
             /* FALL THROUGH */
         } else if (res == DW_DLV_NO_ENTRY) {
+            if (array == NULL) {
+                _dwarf_error_string(dbg,error,DW_DLE_LOCLISTS_ERROR,
+                    "DW_DLE_LOCLISTS_ERROR: "
+                    "no .debug_loclists context available");
+                return DW_DLV_ERROR;
+            }
             loclists_contextnum = 0;
             /* FALL THROUGH */
         } else {
